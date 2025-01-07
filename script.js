22d2d0894a785be86a79d303f4ef7023e2a3b28b
zhengran14 <zhengran14@users.noreply.github.com>
2025-01-07 10:30:46 +0800

    增加tabnine.com

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var DNS2 = "202.106.46.151";
 
 // 自定义规则配置
 const customRules = [
+  "DOMAIN-SUFFIX,tabnine.com,Proxy",
   "DOMAIN-SUFFIX,omnivore.app,Proxy",
   "DOMAIN-SUFFIX,vcpkg.link,Proxy",
   "DOMAIN-SUFFIX,codeproject.com,Proxy",
